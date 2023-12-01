d6118ee015736aafd293c2ec7051aabba5493b2d
Tom Labiausse <tom01lab@gmail.com>
2023-12-01 15:20:26 +0100

    multiprocess synth exp

--- synthetic_exp.py
+++ synthetic_exp.py
@@ -11,10 +11,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+from functools import partial
+from multiprocessing.pool import Pool
+
 from pursuits import Pursuit, SklearnOrthogonalMatchingPursuit, OrthogonalMatchingPursuit
 from dictionary_learning import KSVD
 from synthetic_data import SyntheticData
 from utils import plot_results_synthetic_exp
+
+
+def run_ksvd_synthetic_experiment(
+        n_features: int,
+        n_signals: int,
+        n_atoms: int,
+        sparsity: int,
+        noise_std: float,
+        max_iter: int,
+        pursuit_method: Pursuit,
+        success_threshold: float,
+        logs: bool = False,
+    ) -> int:
+    """
+    Run KSVD experiment on synthetic data and return the success score.
+    """
+
+    if logs:
+        print(f"Exp PID={os.getpid()} just started")
+
+    # create synthetic data
+    data = SyntheticData(n_features=n_features)
+    data.create_synthetic_dictionary(n_atoms=n_atoms, normalize_columns=True, return_dict=False)
+    y = data.create_synthetic_signals(n_signals=n_signals, sparsity=sparsity, noise_std=noise_std, return_signals=True)
+
+    if logs:
+        print(f"Exp PID={os.getpid()} created data")
+
+    # run KSVD
+    ksvd = KSVD(n_atoms=n_atoms, sparsity=sparsity, pursuit_method=pursuit_method, verbose=False)
+    ksvd.fit(y=y, max_iter=max_iter, return_reconstruction=False)
+
+    if logs:
+        print(f"Exp PID={os.getpid()} executed KSVD")
+
+    # compute score
+    score = data.sucess_score(designed_dict=ksvd.dict, threshold=success_threshold)
+
+    if logs:
+        print(f"Exp PID={os.getpid()} finished with score={score}")
+
+    return score
+
+
+def run_ksvd_synthetic_experiment_wrapper(_, **kwargs) -> int:
+    """
+    Wrapper for $run_ksvd_synthetic_experiment$ to be used when multiprocessing with Pool.map for example.
+    """
+    # kwargs["logs"] = True # to log the PID running each experiment
+    return run_ksvd_synthetic_experiment(**kwargs)
 
 
 def mutli_run_ksvd_synthetic_experiment(
@@ -27,6 +80,7 @@ def mutli_run_ksvd_synthetic_experiment(
         n_runs: int = 50,
         pursuit_method: Pursuit = OrthogonalMatchingPursuit,
         success_threshold: float = 0.01,
+        n_process: int = None,
         plot: bool = False,
         save: bool = False,
         save_dir: str = None,
@@ -35,39 +89,59 @@ def mutli_run_ksvd_synthetic_experiment(
     Run KSVD $n_runs$ times with the given parameters and return statistics on the success scores.
     """
 
+    # define noise_std from noise_db
     noise_std = np.sqrt(10 ** (float(noise_db)/20)) if noise_db > 0 else 0
-    scores = np.zeros(n_runs)
-    for run_idx in tqdm(range(n_runs), desc=f"Running experiments with noise {noise_db}dB"):
 
-        # create synthetic data
-        data = SyntheticData(n_features=n_features)
-        data.create_synthetic_dictionary(n_atoms=n_atoms, normalize_columns=True, return_dict=False)
-        y = data.create_synthetic_signals(n_signals=n_signals, sparsity=sparsity, noise_std=noise_std, return_signals=True)
+    # define number of processes to run the experiments
+    n_process = n_process if n_process is not None else os.cpu_count()
 
-        # run KSVD
-        ksvd = KSVD(n_atoms=n_atoms, sparsity=sparsity, pursuit_method=pursuit_method, verbose=False)
-        ksvd.fit(y=y, max_iter=max_iter, return_reconstruction=False)
+    # common parameters for each experiment
+    parameters = {
+        "n_features": n_features,
+        "n_signals": n_signals,
+        "n_atoms": n_atoms,
+        "sparsity": sparsity,
+        "noise_std": noise_std,
+        "max_iter": max_iter,
+        "pursuit_method": pursuit_method,
+        "success_threshold": success_threshold
+    }
 
-        # compute score
-        scores[run_idx] = data.sucess_score(designed_dict=ksvd.dict, threshold=success_threshold)
-
+    if n_process > 1: # parallel
+        with Pool(n_process) as p:
+            pool_result = list(tqdm(
+                p.imap(
+                    partial(
+                        run_ksvd_synthetic_experiment_wrapper, 
+                        **parameters,
+                        ), 
+                    range(n_runs)
+                ), 
+                total=n_runs, 
+                desc=f"Running experiments for noise {noise_db}dB with {n_process} processes"
+            ))
+    else: # sequential
+        pool_result = []
+        for _ in tqdm(range(n_runs), desc=f"Running experiments for noise {noise_db}dB sequentially"):
+            pool_result.append(run_ksvd_synthetic_experiment(**parameters))
+    
+    scores = np.array(pool_result)
     print(f"Average success score = {np.mean(scores)}/{np.std(scores):.2f} (mean/std over {n_runs} runs)")
 
-    if save or plot:
+    # if plot:
+    #     fig, ax = plt.subplots(figsize=(10, 5))
+    #     ax.scatter(np.zeros(n_runs), scores, marker="s", color="black")
+    #     ax.set_title(f"Success scores over {n_runs} runs for the synthetic experiment")
+    #     ax.set_xticks([])
+    #     ax.set_ylabel(f"success score (threshold={success_threshold})")
+    #     if plot:
+    #         plt.show()
 
-        # fig, ax = plt.subplots(figsize=(10, 5))
-        # ax.scatter(np.zeros(n_runs), scores, marker="s", color="black")
-        # ax.set_title(f"Success scores over {n_runs} runs for the synthetic experiment")
-        # ax.set_xticks([])
-        # ax.set_ylabel(f"success score (threshold={success_threshold})")
-
-        # if plot:
-        #     plt.show()
-        if save:
-            save_dir = "outputs/" if save_dir is None else save_dir
-            Path(save_dir).mkdir(parents=True, exist_ok=True)
-            np.save(os.path.join(save_dir, f"success_scores_{noise_db}dB.npy"), scores)
-            # fig.savefig(os.path.join(save_dir, f"success_scores_{noise_db}dB.png"))
+    if save:
+        save_dir = "outputs/" if save_dir is None else save_dir
+        Path(save_dir).mkdir(parents=True, exist_ok=True)
+        np.save(os.path.join(save_dir, f"success_scores_{noise_db}dB.npy"), scores)
+        # fig.savefig(os.path.join(save_dir, f"success_scores_{noise_db}dB.png"))
 
 
 def main():
@@ -86,6 +160,7 @@ def main():
             n_runs = 50,
             pursuit_method = OrthogonalMatchingPursuit,
             success_threshold = 0.01,
+            n_process=1,
             plot = False,
             save = True,
             save_dir = save_dir,
@@ -94,7 +169,7 @@ def main():
     plot_results_synthetic_exp(
         dir=save_dir,
         n_runs=50,
-        success_threshold=0.01
+        success_threshold=0.01,
     )
     
 
